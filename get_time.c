33aaf6366f32de81dd27481d7c157d9e26940a37
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-09-19 11:45:10 -0500

    Update get_time.c

--- get_time.c
+++ get_time.c
@@ -1,9 +1,11 @@
 #include <sys/time.h>
 
-long long current_timestamp() {
-    struct timeval te; 
-    gettimeofday(&te, NULL); // get current time
-    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
-    // printf("milliseconds: %lld\n", milliseconds);
-    return milliseconds;
+long main(void){
+    long long current_timestamp() {
+        struct timeval te; 
+        gettimeofday(&te, NULL); // get current time
+        long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
+        // printf("milliseconds: %lld\n", milliseconds);
+        return milliseconds;
+    }
 }
