b1a0ef909222f2c37ad2ab26bfcbf04844db4f54
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-09-19 11:46:49 -0500

    Update get_time.c

--- get_time.c
+++ get_time.c
@@ -1,6 +1,6 @@
 #include <sys/time.h>
 
-long main(void){
+int main(void){
     struct timeval te; 
     gettimeofday(&te, NULL); // get current time
     long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
