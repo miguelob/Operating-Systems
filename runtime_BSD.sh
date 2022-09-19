315c65e945f08968d422badd3e744ac2d47e5eb7
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-09-19 11:31:13 -0500

    Update runtime_BSD.sh

--- runtime_BSD.sh
+++ runtime_BSD.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-START=(`date +%s`*1000)
+START=((`date +%s`*1000))
 for i in `seq 1 $1`;
 do 
    exec ./a.out $i &
 done
-END=(`date +%s`*1000)
+END=((`date +%s`*1000))
 DELTA=$((((END-START))/1000000))
 
 echo $START $END $DELTA
