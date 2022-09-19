6dfc717b4654c234afbfec0d7326380ae8ca967d
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-09-19 11:49:26 -0500

    Update get_time.c

--- get_time.c
+++ get_time.c
@@ -3,7 +3,7 @@
 int main(void){
     struct timeval te; 
     gettimeofday(&te, NULL); // get current time
-    long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // calculate milliseconds
     printf("milliseconds: %lld\n", milliseconds);
     //return milliseconds;
     return 0;
