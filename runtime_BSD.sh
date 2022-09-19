5b8e1d4a9225183b456d9bb43db3f8ec7ee8e076
Miguel Oleo <miguel.oleo@alu.comillas.edu>
2022-09-19 12:21:27 -0500

    Update runtime_BSD.sh

--- runtime_BSD.sh
+++ runtime_BSD.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 START=`exec ./time.out`
-echo $START
 for i in `seq 1 $1`;
 do 
    exec ./a.out $i &
 done
 END=`exec ./time.out`
-echo $END
 DELTA=$((END-START))
 
-echo $START $END $DELTA
+echo Time Elapsed $DELTA
