e48d6e366be481f555c6dd1f0aaf18d3201cc5f0
NicolasArmero <55202512+NicolasArmero@users.noreply.github.com>
2022-10-16 15:34:10 -0500

    Create PTrace.c
    Trace Write call

---
+++ PTrace.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{   
+    pid_t child;
+    long  orig_eax;
+    child = fork();
+    if(child == 0)
+    {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        char *args[]={"./a.out",NULL};
+        execvp(args[0],args);
+    }
+    else
+    {
+        wait(NULL);
+        orig_eax = ptrace(PTRACE_PEEKUSER,child, 4 * ORIG_EAX, NULL);
+        printf("The child made a system call %ld\n", orig_eax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
+    return 0;
+}
